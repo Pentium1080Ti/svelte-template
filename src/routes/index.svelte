e440f4495ddc8e22506f57ab0c64171b994befff
Old Remix <pentium1080ti@protonmail.com>
2022-06-10 20:11:29 +0100

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,4 @@
+<div class="py-5 flex flex-col text-center">
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a class="underline" target="_blank" rel="noreferrer noopener" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>    
+</div>
